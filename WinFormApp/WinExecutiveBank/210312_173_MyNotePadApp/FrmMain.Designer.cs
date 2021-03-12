14b4a98ee5a9d957f808e8dc0c705fa501043f41
SeoDongWoo1216 <dw6642@naver.com>
2021-03-12 15:56:40 +0900

    0312 15:56 윈폼(174 메모장까지)

---
+++ WinFormApp/WinExecutiveBank/210312_173_MyNotePadApp/FrmMain.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace _210312_173_MyNotePadApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuNewFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuOpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuSaveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.MnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.편집ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말HToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.TxtMain = new System.Windows.Forms.RichTextBox();
+            this.DlgOpenText = new System.Windows.Forms.OpenFileDialog();
+            this.DlgSaveText = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.편집ToolStripMenuItem,
+            this.도움말HToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MnuNewFile,
+            this.MnuOpenFile,
+            this.MnuSaveFile,
+            this.끝내기ToolStripMenuItem,
+            this.MnuExit});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.파일ToolStripMenuItem.Text = "파일(&F)";
+            // 
+            // MnuNewFile
+            // 
+            this.MnuNewFile.Name = "MnuNewFile";
+            this.MnuNewFile.Size = new System.Drawing.Size(155, 22);
+            this.MnuNewFile.Text = "새로 만들기(&N)";
+            this.MnuNewFile.Click += new System.EventHandler(this.MnuNewFile_Click);
+            // 
+            // MnuOpenFile
+            // 
+            this.MnuOpenFile.Name = "MnuOpenFile";
+            this.MnuOpenFile.Size = new System.Drawing.Size(155, 22);
+            this.MnuOpenFile.Text = "열기(&O)";
+            this.MnuOpenFile.Click += new System.EventHandler(this.MnuOpenFile_Click);
+            // 
+            // MnuSaveFile
+            // 
+            this.MnuSaveFile.Name = "MnuSaveFile";
+            this.MnuSaveFile.Size = new System.Drawing.Size(155, 22);
+            this.MnuSaveFile.Text = "저장(&S)";
+            this.MnuSaveFile.Click += new System.EventHandler(this.MnuSaveFile_Click);
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(152, 6);
+            // 
+            // MnuExit
+            // 
+            this.MnuExit.Name = "MnuExit";
+            this.MnuExit.Size = new System.Drawing.Size(180, 22);
+            this.MnuExit.Text = "종료(&X)";
+            this.MnuExit.Click += new System.EventHandler(this.MnuExit_Click);
+            // 
+            // 편집ToolStripMenuItem
+            // 
+            this.편집ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MnuCopy,
+            this.MnuPaste});
+            this.편집ToolStripMenuItem.Name = "편집ToolStripMenuItem";
+            this.편집ToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.편집ToolStripMenuItem.Text = "편집(&E)";
+            // 
+            // MnuCopy
+            // 
+            this.MnuCopy.Name = "MnuCopy";
+            this.MnuCopy.Size = new System.Drawing.Size(180, 22);
+            this.MnuCopy.Text = "복사(&C)";
+            this.MnuCopy.Click += new System.EventHandler(this.MnuCopy_Click);
+            // 
+            // MnuPaste
+            // 
+            this.MnuPaste.Name = "MnuPaste";
+            this.MnuPaste.Size = new System.Drawing.Size(180, 22);
+            this.MnuPaste.Text = "붙여넣기(&P)";
+            this.MnuPaste.Click += new System.EventHandler(this.MnuPaste_Click);
+            // 
+            // 도움말HToolStripMenuItem
+            // 
+            this.도움말HToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MnuAbout});
+            this.도움말HToolStripMenuItem.Name = "도움말HToolStripMenuItem";
+            this.도움말HToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.도움말HToolStripMenuItem.Text = "도움말(&H)";
+            // 
+            // MnuAbout
+            // 
+            this.MnuAbout.Name = "MnuAbout";
+            this.MnuAbout.Size = new System.Drawing.Size(166, 22);
+            this.MnuAbout.Text = "프로그램 정보(&A)";
+            this.MnuAbout.Click += new System.EventHandler(this.MnuAbout_Click);
+            // 
+            // TxtMain
+            // 
+            this.TxtMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TxtMain.Location = new System.Drawing.Point(0, 24);
+            this.TxtMain.Name = "TxtMain";
+            this.TxtMain.Size = new System.Drawing.Size(800, 426);
+            this.TxtMain.TabIndex = 1;
+            this.TxtMain.Text = "";
+            this.TxtMain.TextChanged += new System.EventHandler(this.TxtMain_TextChanged);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyNotePad";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MnuNewFile;
+        private System.Windows.Forms.ToolStripMenuItem MnuOpenFile;
+        private System.Windows.Forms.ToolStripMenuItem MnuSaveFile;
+        private System.Windows.Forms.ToolStripSeparator 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MnuExit;
+        private System.Windows.Forms.ToolStripMenuItem 편집ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MnuCopy;
+        private System.Windows.Forms.ToolStripMenuItem MnuPaste;
+        private System.Windows.Forms.ToolStripMenuItem 도움말HToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MnuAbout;
+        private System.Windows.Forms.RichTextBox TxtMain;
+        private System.Windows.Forms.OpenFileDialog DlgOpenText;
+        private System.Windows.Forms.SaveFileDialog DlgSaveText;
+    }
+}
+
---
+++ WinFormApp/WinExecutiveBank/210312_173_MyNotePadApp/FrmMain.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _210312_173_MyNotePadApp
+{
+    public partial class FrmMain : Form
+    {
+        public bool IsModify { get; set; }    // 수정했는지 안했는지를 bool로 판별
+
+        private const string firstFileName = "noname.txt";
+
+        private string currFileName = firstFileName;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void MnuNewFile_Click(object sender, EventArgs e)
+        {
+            // TODO : 만약 작업중인 파일 있으면 저장 처리
+            ProcessSaveFileBeforeClose();
+
+            TxtMain.Text = "";
+            IsModify = false;
+            currFileName = firstFileName;
+            this.Text = $"{currFileName} - 내 메모장 ";
+        }
+
+        private void MnuOpenFile_Click(object sender, EventArgs e)
+        {
+            ProcessSaveFileBeforeClose();
+
+            DlgOpenText.ShowDialog();
+            currFileName = DlgOpenText.FileName;
+
+            this.Text = $"{currFileName} - 내 메모장 ";
+
+            try
+            {
+                StreamReader sr = File.OpenText(currFileName);
+                TxtMain.Text = sr.ReadToEnd();
+
+                IsModify = false;
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MnuSaveFile_Click(object sender, EventArgs e)
+        {
+            if(currFileName == firstFileName)
+            {
+                DlgSaveText.Filter = "Text file (*.txt) | *.txt | Log file(*.log) | *.log";
+                if(DlgSaveText.ShowDialog() == DialogResult.OK)
+                {
+                    currFileName = DlgSaveText.FileName;
+                }
+            }
+
+            StreamWriter sw = File.CreateText(currFileName);
+            sw.WriteLine(TxtMain.Text);
+
+            IsModify = false;
+            sw.Close();
+
+            this.Text = $"{currFileName} - 내 메모장 ";
+        }
+
+        private void MnuExit_Click(object sender, EventArgs e)
+        {
+            ProcessSaveFileBeforeClose();
+            Environment.Exit(0);
+        }
+
+        private void ProcessSaveFileBeforeClose()
+        {
+            if (IsModify)
+            {
+                DialogResult answer = MessageBox.Show("변경사항이 있습니다. 저장하시겠습니까?", "저장",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if(answer == DialogResult.Yes)
+                {
+                    if(currFileName == firstFileName)  // 파일 이름을 지정하지 않았다면
+                    {
+                        if(DlgSaveText.ShowDialog() == DialogResult.OK)
+                        {
+                            StreamWriter sw = File.CreateText(DlgSaveText.FileName);
+                            sw.WriteLine(TxtMain.Text);
+                            sw.Close();
+                        }
+                        else  // 파일 이름이 지정되어 있다면
+                        {
+                            StreamWriter sw = File.CreateText(currFileName);
+                            sw.WriteLine(TxtMain.Text);
+                            sw.Close();
+                        }
+                    }
+                }
+
+            }
+        }
+      
+      
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            DlgSaveText.Filter = "Text file (*.txt) | *.txt | Log file(*.log) | *.log";
+            this.Text = $"{currFileName} - 내 메모장 ";
+            IsModify = false;
+        }
+
+        private void TxtMain_TextChanged(object sender, EventArgs e)
+        {
+            IsModify = true;
+            this.Text = $"{currFileName} * - 내 메모장 ";
+        }
+
+        private void MnuCopy_Click(object sender, EventArgs e)
+        {
+            var contents = ActiveControl as RichTextBox;
+            if(contents != null)
+            {
+                Clipboard.SetDataObject(contents.SelectedText);
+                MessageBox.Show(contents.SelectedText);
+            }
+        }
+
+        private void MnuPaste_Click(object sender, EventArgs e)
+        {
+            var contents = ActiveControl as RichTextBox;
+            if (contents != null)
+            {
+                IDataObject data = Clipboard.GetDataObject();
+                contents.SelectedText = data.GetData(DataFormats.Text).ToString();
+                IsModify = true;
+            }
+        }
+
+        private void MnuAbout_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("메모장 v1.0입니다");
+            var form = new AboutThis();
+            form.ShowDialog();
+        }
+    }
+}
